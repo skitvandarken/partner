bb0d745731b186e8fbec00faab0abbcdcdd4a1ed
Lukeba Jorge <lukeba.jorge@angolacables.co.ao>
2025-01-08 11:31:37 +0100

    removido o firebase e o supabase, novamente funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './paginas/inicio/inicio.component';
+import { AuthComponent } from './paginas/auth/auth.component';
 
 export const routes: Routes = [
 
     {path:'', component:InicioComponent},
+    {path:'auth', component: AuthComponent}
 
 ];
--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../layout/menu/menu.component';
-import { VideocardComponent } from '../../layout/videocard/videocard.component';
-import { SliderComponent } from '../../layout/slider/slider.component';
 import { BeneficiosComponent } from '../../layout/beneficios/beneficios.component';
 import { AngolacablesComponent } from '../../layout/angolacables/angolacables.component';
 import { CategoriasComponent } from '../../layout/categorias/categorias.component';
-import { CardsComponent } from '../../layout/cards/cards.component';
 import { ServicosComponent } from '../../layout/servicos/servicos.component';
 import { RodapeComponent } from '../../layout/rodape/rodape.component';
 
@@ -13,12 +10,10 @@ import { RodapeComponent } from '../../layout/rodape/rodape.component';
   selector: 'app-inicio',
   imports: [
     MenuComponent,
-    VideocardComponent,
-    SliderComponent,
+   
     BeneficiosComponent,
     AngolacablesComponent,
     CategoriasComponent,
-    CardsComponent,
     ServicosComponent,
     RodapeComponent
   ],
